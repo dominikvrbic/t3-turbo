345c44bbb9c0a14c70934c7beef415c1da4797a0
dominikvrbic <dominikvrbic01@gmail.com>
2022-12-06 14:29:34 +0100

    fixes

--- packages/api/src/auth/router.test.ts
+++ packages/api/src/auth/router.test.ts
@@ -7,6 +7,7 @@ import { authRouter } from './router';
 
 beforeAll(async () => {
   const ctx = await createContext();
+  await ctx.prisma.user.deleteMany();
   await ctx.prisma.user.create({
     data: {
       name: 'test',
